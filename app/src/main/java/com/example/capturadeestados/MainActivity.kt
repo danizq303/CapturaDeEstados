928c3209e1da8c80106464f61908538803a057c8
danizq303 <danizq303@alu.centroafuera.es>
2022-11-15 12:21:54 +0100

    Initial commit

---
+++ app/src/main/java/com/example/capturadeestados/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.capturadeestados
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    private var onCreate = 0
+    private var onStart = 1
+    private var onResume = 2
+    private var onPause = 3
+    private var onStop = 4
+    private var onDestroy = 5
+    private var onRestart = 6
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        println("onCreate $onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        println("onStart $onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        println("onResume $onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        println("onPause $onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        println("onStop $onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        println("onDestroy $onDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        println("onRestart $onRestart")
+    }
+}
